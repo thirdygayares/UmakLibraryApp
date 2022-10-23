82d01243f965cd670c4ea30ce8dc897aa3dc6aef
thirdy454 <gayares@gmail.com>
2022-10-23 22:44:39 +0800

    adding gilroy fonts add login activity add book details

---
+++ app/src/main/java/com/firetera/umaklibraryapp/BookDetails.java
@@ -0,0 +1,14 @@
+package com.firetera.umaklibraryapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class BookDetails extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_book_details);
+    }
+}
