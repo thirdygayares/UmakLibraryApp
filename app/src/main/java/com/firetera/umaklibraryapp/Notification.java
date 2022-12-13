19618fac4c33fa95164a22fdf96bc8da7790bd45
thirdy454 <gayares@gmail.com>
2022-12-13 20:27:45 +0800

    add bottom navigation bar

---
+++ app/src/main/java/com/firetera/umaklibraryapp/Notification.java
@@ -0,0 +1,26 @@
+package com.firetera.umaklibraryapp;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class Notification extends Fragment {
+
+    View view;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        view = inflater.inflate(R.layout.activity_notification, container, false);
+
+        return view;
+
+    }
+}
